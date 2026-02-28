60b8a6451a9ed4413ca8b63504ef123d2dacb013
sonam-niit <sonam.gravity@gmail.com>
2026-02-28 14:20:34 +0530

    Terraform with cicd

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "remote-backend-s3-sonam"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    # dynamodb_table = "table_name" # used for Locking state when multiple users using
+  }
+}
 resource "aws_instance" "example" {
   ami = "ami-0f3caa1cf4417e51b"
   instance_type= "t2.micro"
