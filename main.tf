dcf64b8060cd1d2d789f215cd3f7286c0207a802
sonam-niit <sonam.gravity@gmail.com>
2026-02-28 14:20:23 +0530

    Terraform with cicd

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0f3caa1cf4417e51b"
+  instance_type= "t2.micro"
+
+  tags = {
+    Name = "myapp-${var.env}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "env" {
+  description = "Environment Variable"
+  type = string
+  default = "dev"
+}
